ef43bbee3f6202ee0f178d8a2815b43f85209f69
nvnchandran8 <nvnchandran8@gmail.com>
2020-10-08 14:47:57 +0530

    slide code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SlideNavigation from "./SlideNavigation";
+import "./style.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <SlideNavigation slideShowArray={[{name:'name1',content:'content1'},{name:'name2',content:'content2'},{name:'name3',content:'content3'},{name:'name4',content:'content4'},{name:'name5',content:'content5'}]} />
     </div>
   );
 }
-
-export default App;
